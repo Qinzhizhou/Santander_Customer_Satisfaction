aabaf2d20faed4bea5bdc6faa6538335f116f702
Qinzhizhou <qinzhiz@outlook.com>
2023-01-02 22:35:00 +0800

    second commit with score 0.72

--- data_processing/process.py
+++ data_processing/process.py
@@ -7,6 +7,7 @@ import numpy as np # linear algebra
 import pandas as pd # data processing, CSV file I/O (e.g. pd.read_csv)
 
 class PROCESSING:
+
     def process():
         print("aaa")
         train = pd.read_csv("../data/input/train.csv")
